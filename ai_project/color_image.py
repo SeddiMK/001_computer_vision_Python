de4f165805122a98a97935297a32e6a8a363680f
Max <93527867+SeddiMK@users.noreply.github.com>
2023-04-10 18:13:35 +0700

    Add files via upload

--- ai_project/color_image.py
+++ ai_project/color_image.py
@@ -5,6 +5,12 @@ img = cv2.imread('img/77_leopard.jpg')
 
 # img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+r, g, b = cv2.split(img)  # разделение слоев
+
+img = cv2.merge([b, g, r])  # объединение слоев
 
 cv2.imshow('Photo_Proba', img)
 cv2.waitKey(2 * 1000)
